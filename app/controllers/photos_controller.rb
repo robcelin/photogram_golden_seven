f0068da2f6ec76869ff490b82b13fae22bbd3707
robcelin <rob.celin@kellogg.northwestern.edu>
2017-04-25 09:57:50 -0500

    1st 1.5 hours of class

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,21 @@
+class PhotosController < ApplicationController
+  def show
+    #The params hash looks like this :[the_id_number"=>"1"}]
+
+    id_number = params["the_id_number"]
+    p = Photo.find(id_number)
+
+    @the_caption = p.caption
+    @the_image_url = p.source
+    @created_at = p.created_at
+
+    render("photos/show.html.erb")
+  end
+
+  def index
+    @all_photos = Photo.all
+    
+    render("photos/index.html.erb")
+  end
+
+end
